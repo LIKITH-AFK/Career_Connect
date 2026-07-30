c394abe6199dc1a7e30634f91bd41277561bbd11
LIKITH-AFK <likithsairam15@gmail.com>
2026-07-30 12:54:35 +0530

    Update BASE_URL to Render live backend URL

--- js/api.js
+++ js/api.js
@@ -1,7 +1,7 @@
 // ================================
 // CONSTANTS
 // ================================
-const BASE_URL = "http://localhost:3000";
+const BASE_URL = "https://career-connect-backend-api.onrender.com";
 
 // ================================
 // FUNCTIONS
